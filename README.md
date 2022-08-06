4fa30e30c6938919a7d22c8c5cd44807a3b2d84b
Rashid Mingazov <rashid.mingazov@mircod.com>
2022-08-06 21:48:11 +0300

    added web server

---
+++ README.md
@@ -0,0 +1,6 @@
+# Dynamic load balancer written in Python 3.10.5
+
+## Env variables:
+    - HOST: "0.0.0.0"
+    - PORT: 8888
+    - UPSTREAM-{index: int - starting with 0}: json {"domain": "app.localhost", "pool": ["host_1:port_1", "host_2:port_2", "host_3:port_3"], "default": 1}
---
+++ src/main.py
@@ -0,0 +1,58 @@
+import socket
+import os
+
+try:
+    from http_parser.parser import HttpParser
+except ImportError:
+    from http_parser.pyparser import HttpParser
+
+
+HOST, PORT = os.getenv("HOST", ""), os.getenv("PORT", 80)
+
+
+def send_http_request():
+
+
+def main():
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind((HOST, PORT))
+    listen_socket.listen(1)
+    print('Serving HTTP on port {PORT} ...')
+
+    while True:
+        client_connection, client_address = listen_socket.accept()
+        p = HttpParser()
+        body = []
+        raw_request = b""
+
+        while True:
+            request_data = client_connection.recv(1024)
+            raw_request += request_data
+
+            if not request_data:
+                break
+
+            recved = len(request_data)
+            nparsed = p.execute(request_data, recved)
+
+            assert recved == nparsed
+
+            if p.is_partial_body():
+                body.append(p.recv_body())
+
+            if p.is_message_complete():
+                break
+
+        addressed_host = p.get_headers()["Host"]
+
+        http_response = b"""
+        HTTP/1.1 200 OK
+        bebra
+        """
+        client_connection.sendall(http_response)
+        client_connection.close()
+
+
+if __name__ == "__main__":
+    main()
